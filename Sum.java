383d207385024baa90dc0ddc1db4159f17f83df1
SiwaThursupap <notsiwa1@gmail.com>
2025-07-25 11:18:07 +0700

    First commit

---
+++ Sum.java
@@ -0,0 +1,11 @@
+public class Sum {
+     public static void main (String[]arg){ //ตรงนี้จะขึ้นเสมอถ้าใช้นามสกุลไฟล์ถูก//
+      int num1 = 4 ; // int คือตัวประกาศตัวแปร //
+      int num2 = 6 ;
+      int sum = num1 + num2 ; 
+      System.out.println("Sum of "+num1+" and "+num2+" is "+sum); //ตรงนี้อย่าลืมตัวใหญ่ตัวแรกเสมอ//
+
+    }
+}
+// javac ชื่อไฟล์.java = ใช้สำหรับ compile//
+// java ชื่อไฟล์ = ใช้สำหรับ run (ไม่ต้องมีสกุลไฟล์)
