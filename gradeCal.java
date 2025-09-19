2be17b6f8548cee3d0d6553ff0406c403f67bfa3
SiwaThursupap <notsiwa1@gmail.com>
2025-09-19 11:57:36 +0700

    firstcommit

---
+++ gradeCal.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class gradeCal {
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter You Name : ");
+        String name = scanner.nextLine();
+        System.out.print("Enter You Score : ");
+        int score = scanner.nextInt();
+        System.out.println("Name : "+ name);
+        System.out.println("Score : "+ score);
+        if (score >= 80) {
+            System.out.println("Grade : A ");
+        }
+        else if (score >= 70) {
+            System.out.println("Grade : B ");
+        }
+        else if (score >= 60) {
+            System.out.println("Grade : C ");
+        }
+        else if (score >= 50) {
+            System.out.println("Grade : D ");
+        }
+        else {
+            System.out.println("Grade : E ");
+        }
+    }
+}
---
+++ temstatus.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class temstatus {
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter You Temperature in Celsius : ");
+        int celsius = scanner.nextInt();
+        System.out.println("Temperature : "+ celsius);
+        if (celsius > 35){
+            System.out.println("Status : Hot");
+        }
+        else if (celsius >= 20){
+            System.out.println("Status : Normal");
+        }
+        else{
+            System.out.println("Status : Cold");
+        }
+    }
+}
